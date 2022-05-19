0841d501167c4256ba90a936f1e349221ce5c9ef
jayasooriya <sjsooriya96@gmail.com>
2022-05-19 21:46:07 +0530

    local starged storage added for todo list

--- src/features/todo/todo-slice.ts
+++ src/features/todo/todo-slice.ts
@@ -5,7 +5,7 @@ interface TodoState {
   isCompleted: Boolean;
 }
 
-const initialState: TodoState[] = [];
+const initialState: TodoState[] = getLocalStorage();
 const todoSlice = createSlice({
   name: "todo",
   initialState,
@@ -13,16 +13,27 @@ const todoSlice = createSlice({
     // Add ToDo
     todoAdded(state: TodoState[], action: PayloadAction<string>) {
       state.push({ title: action.payload, isCompleted: false });
+      saveOnLocal(state);
     },
 
     todoCompleted(state: TodoState[], action: PayloadAction<number, string>) {
       state[action.payload].isCompleted = true;
+      saveOnLocal(state);
     },
     removeTodo(state: TodoState[], action: PayloadAction<number>) {
       state.splice(action.payload, 1);
+      saveOnLocal(state);
     },
   },
 });
 
+async function saveOnLocal(list: TodoState[]) {
+  localStorage.setItem("todoList", JSON.stringify(list));
+  await console.log(localStorage.getItem("todoList"));
+}
+
+function getLocalStorage(): TodoState[] {
+  return JSON.parse(localStorage.getItem("todoList") ?? "[]");
+}
 export const { todoAdded, todoCompleted, removeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
